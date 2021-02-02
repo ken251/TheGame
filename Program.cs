eca1c23829e14d698d92511c9ecb31302161436d
Kennedy Wambua <kennywambu254@gmail.com>
2021-02-02 14:53:04 +0300

    v1.0.0

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel;//classes 4 basic control behaviour
+using System.Drawing;//obviously drawing 
+using System.Windows.Forms;
+
+using Microsoft.DirectX;
+using Microsoft.DirectX.Direct3D;
+
+
+
+
+namespace TheGame
+{
+    public class MainClass : Form
+    {
+
+      
+        static void Main(string[] args)
+        {
+         
+        
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
